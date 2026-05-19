d7f126630ec153d50904d31be5abfe621d8b322f
farmeen <farmeenq125@gmail.com>
2026-05-19 09:57:09 +0500

    Initial commit

---
+++ Library.py
@@ -0,0 +1,45 @@
+from tkinter import*
+from tkinter import ttk
+import random
+from datetime import datetime
+import tkinter.messagebox
+
+class Library:
+
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Library Management System")
+        self.root.geometry("1350x750+0+0")
+        self.root.configure(background="powder blue")
+
+        #=================Frame=================
+
+        MainFrame=Frame(self.root)
+        MainFrame.grid()
+
+        TitleFrame=Frame(MainFrame, width=1350, padx=20, bd=20, relief=RIDGE)
+        TitleFrame.pack(side=TOP)
+
+        self.lblTitle=Label(TitleFrame, width=39, font=('arial', 40, 'bold'), text="\tLibrary Management System\t", padx=12)
+        self.lblTitle.grid()
+
+        ButtonFrame=Frame(MainFrame, width=1350, height=50, padx=20, relief=RIDGE)
+        ButtonFrame.pack(side=BOTTOM)
+
+        FrameDetails=Frame(MainFrame, bd=20, width=1350, height=400, padx=20, relief=RIDGE)
+        FrameDetails.pack(side=BOTTOM)
+
+        DataFrame=Frame(FrameDetails, bd=20, width=800, height=300, padx=20, relief=RIDGE)
+        DataFrame.pack(side=BOTTOM)
+
+        DataFrameLEFT=LabelFrame(DataFrame, bd=10, width=400, height=300, padx=20, relief=RIDGE
+                                 , font=('arial', 12, 'bold'), text="Library Membership Info:",)
+        DataFrameLEFT.pack(side=LEFT)
+
+        DataFrameRIGHT=LabelFrame(DataFrame, bd=10, width=400, height=300, padx=20, relief=RIDGE
+                                  , font=('arial', 12, 'bold'), text="Book Details:",)
+        DataFrameRIGHT.pack(side=RIGHT)
+
+        
+        
+
